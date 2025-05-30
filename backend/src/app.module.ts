a69c4ca338d481826b3daa32bf6ac54672514554
thePiece <christiankubik01@gmail.com>
2025-05-30 12:19:31 +0200

    Adding api limit to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,33 @@
-import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ArchAiModule } from 'src/modules/ArchAiModule';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-
     imports: [
-      // set configuration from ".env" file
+        // set configuration from ".env" file
         ConfigModule.forRoot({
             isGlobal: true,
             envFilePath: '.env',
         }),
+        // Add api limit with throttler
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    ttl: 86400,
+                    limit: 100,
+                },
+            ],
+        }),
         ArchAiModule,
     ],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard, // Bindet den Guard global für die App
+        },
+    ],
 })
 export class AppModule {}

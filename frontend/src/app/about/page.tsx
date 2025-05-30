70e70687199b57428ca0b34e744775007a9e8815
thePiece <christiankubik01@gmail.com>
2025-05-30 11:58:45 +0200

    Update About Page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-import BackButton from "@/components/BackButton";
+import BackButton from '@/components/BackButton';
 
 export default function About() {
   return (
     <div className="flex-col flex justify-center w-full min-h-[80vh] text-left px-4 sm:px-4 md:px-8 lg:px-24 xl:px-32 animate-slide-in-right-to-left">
       <div className="my-10">
-        <BackButton/>
+        <BackButton />
       </div>
 
       <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
@@ -16,8 +16,11 @@ export default function About() {
           What is ArchAi?
         </h2>
         <p className="text-base sm:text-lg leading-relaxed">
-          ArchAi is a platform designed to help users to transform their ideas
-          into secure, efficient cloud architecture solutions.
+          ArchAi is a platform developed to support users in transforming
+          conceptual ideas into secure and efficient cloud architecture
+          solutions. It leverages advanced automation techniques to bridge the
+          gap between user requirements and technical cloud infrastructure
+          design.
         </p>
       </section>
 
@@ -26,8 +29,11 @@ export default function About() {
           How does it work?
         </h2>
         <p className="text-base sm:text-lg leading-relaxed">
-          ArchAi uses llm-driven automation to assist in planning, and
-          generation of cloud architectures.
+          ArchAi employs large language model (LLM)-driven automation to assist
+          in the planning and automatic generation of cloud architectures,
+          ensuring scalability, security, and cost-efficiency. The platform
+          interprets high-level user input and generates optimized
+          infrastructure blueprints based on best practices.
         </p>
       </section>
 
@@ -36,9 +42,13 @@ export default function About() {
           Background
         </h2>
         <p className="text-base sm:text-lg leading-relaxed">
-          This platform is developed as part of a university project, aimed at
-          exploring the intersection of software engineering, AI, and cloud
-          architectures technologies.
+          The development of ArchAi is part of a university research project
+          that explores the integration of software engineering methodologies,
+          artificial intelligence, and cloud computing technologies. The
+          platform aims to simplify the complex task of cloud architecture
+          design, making it accessible for users without extensive cloud
+          expertise while promoting innovative and reliable infrastructure
+          solutions.
         </p>
       </section>
     </div>
